89b8df7ceec5465f21fdd6ed2c775b4e15faea5e
mehmetkaraha <eminks@mail.ru>
2017-11-23 11:03:51 +0200

    Revert "Dosyaların yüklenmesi"
    This reverts commit 279316d94d48e77846c75a8e0bc8ee520375808f.

--- mz/calt.php
+++
@@ -1,8 +0,0 @@
-<?php 
-if ($cahce == 1){
-$fp = fopen($cachefile, 'w+');  
-fwrite($fp, ob_get_contents());  
-fclose($fp);  
-ob_end_flush();  
-}
-?>
--- mz/temalar/celal/calt.php
+++
@@ -1,6 +0,0 @@
-<?php 
-////$fp = fopen($cachefile, 'w+');  
-////fwrite($fp, ob_get_contents());  
-////fclose($fp);  
-////ob_end_flush();  
-?>
